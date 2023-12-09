54568b6c4faad30430b4964bc450445ac0608299
Sam Tanner <samdtanner1989@gmail.com>
2023-12-09 11:55:59 +0000

    added basic login data

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from crypt import methods
 from doctest import debug
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
+
 
 @app.route("/")
 def index():
@@ -16,6 +21,38 @@ def kingdoms():
 def projects():
     return render_template("projects.html")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login succesful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+    session.pop("user", None)
+    
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run()
